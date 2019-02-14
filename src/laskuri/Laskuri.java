55e74c84f143b96266592b56e29b2b1050899d08
samsobon <sami.sobori@gmail.com>
2019-02-14 15:02:32 +0200

    Add files via upload

---
+++ src/laskuri/Laskuri.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskuri;
+
+/**
+ *
+ * @author samis
+ */
+public class Laskuri{
+  /* laskuriolion kentan arvoa voi kasvattaa yhdella kutsumalla metodia
+     lisaa(). Arvoa voi tarkastella ja sen voi nollata.                 */
+	private int tapahtumia;
+        public Laskuri(int alkuarvo){ 
+        // konstruktori, kuormitettu. Alkuarvon tulee olla >= 0.
+		if(alkuarvo < 0)
+			return;
+		this.tapahtumia = alkuarvo;
+	}
+	public Laskuri(){
+		this.tapahtumia = 0;
+	}
+	
+  	public void lisaa(){
+    		this.tapahtumia = this.tapahtumia + 1; 
+  	}
+        public void lisaa(int lisays){
+            this.tapahtumia = this.tapahtumia + lisays;
+        }
+  	public int lukema(){
+    		return this.tapahtumia;
+  	}
+  	public void nollaa(){
+    		this.tapahtumia = 0;
+  	}
+	public String toString(){
+		return "Lukema: " + this.tapahtumia;
+	}
+        public static void main(String[] args) {
+        Laskuri laskurini = new Laskuri(5);
+        System.out.println(laskurini);
+        }
+
+}
+
